5884cb57a721a91fa833b66e6e577a7151e065df
chenliang <chenliangboy@hotmail.com>
2018-02-27 17:15:49 +0800

    expresss-session 登录退出

--- septer-doc/nodejs/app/app.js
+++ septer-doc/nodejs/app/app.js
@@ -8,6 +8,7 @@ var ejs = require('ejs');
 var index = require('./routes/index');
 var users = require('./routes/users');
 
+var session = require('express-session');
 var app = express();
 
 // view engine setup
@@ -23,6 +24,27 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+
+app.use(session({  
+  resave: true, // don't save session if unmodified  
+  saveUninitialized: false, // don't create session until something stored  
+  secret: 'love'  
+}));  
+
+app.use(function(req,res,next){  
+  if (!req.session.user) {  
+      if(req.url=="/login"){  
+          next();//如果请求的地址是登录则通过，进行下一个请求  
+      }  
+      else  
+      {  
+          res.redirect('/login');  
+      }  
+  } else if (req.session.user) {  
+      next();  
+  }  
+}); 
+
 app.use('/', index);
 app.use('/users', users);
 
@@ -32,6 +54,11 @@ app.use(function(req, res, next) {
   err.status = 404;
   next(err);
 });
+
+
+ 
+
+
 
 // error handler
 app.use(function(err, req, res, next) {
--- septer-doc/nodejs/app/routes/index.js
+++ septer-doc/nodejs/app/routes/index.js
@@ -6,4 +6,32 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.get("/login",function(req, res, next){
+	res.render('login');
+});
+
+router.post("/login",function(req, res, next){
+
+	console.log(req.body.username+":" + req.body.password);
+	var username = req.body.username;
+	var password = req.body.password;
+
+	if(username=="chenliang" && password=="123456"){  
+		var user = {'username':'chenliang',"password":"123456"};
+        req.session.user = user;  
+        res.redirect('/');  
+    }  
+    else  
+    {  
+        res.redirect('/login');  
+    }  
+
+
+});
+
+router.get('/logout',function(req,res){  
+    req.session.user = null;  
+    res.redirect('/login');  
+});  
+
 module.exports = router;
